a39e9b79fa79b1c42a5c88ea1a8d04ac30950412
Lovmong <1967471283@qq.com>
2023-12-08 09:59:38 +0800

    change rgb pin, pwm freq

--- src/rgb.h
+++ src/rgb.h
@@ -6,24 +6,19 @@
 
 /*---------------------------------------------------------------
     rgb: (PWMB)
-        LED_B -> PWM5 -> P2.0
+        LED_R -> PWM5 -> P2.0
         LED_G -> PWM6 -> P2.1
-        LED_R -> PWM7 -> P2.2
+        LED_B -> PWM7 -> P2.2
 ---------------------------------------------------------------*/
-#define LED_B P20
+#define LED_R P20
 #define LED_G P21
-#define LED_R P22
+#define LED_B P22
 
 #define LED_USE_PWM 1
-#define MAIN_Fosc 12000000L // 定义主时钟 12MHz
+#define MAIN_Fosc 35000000L // 定义主时钟 35MHz
 
-// pwm频率 = 系统时钟/（（预分频+1）/ （自动重装载值arr+1））= 12MHz / 2 / 6000 = 1KHz
-// #define _PWMB_PSCR 2      // 预分频 2
-// #define _PWMB_PERIOD 6000 // PWM周期 (PWMB_ARR自动重装值) 6000
-
-// pwm频率 = 系统时钟/（（预分频+1）/ （自动重装载值arr+1））= 12MHz / 2 / 600 = 10KHz
-#define _PWMB_PSCR 2     // 预分频 2
-#define _PWMB_PERIOD 600 // PWM周期 (PWMA_ARR自动重装值) 600
+#define _PWMB_PSCR 4     // 预分频 4
+#define _PWMB_PERIOD 875 // PWM周期 (PWMB_ARR自动重装值) 875
 
 void rgbInit();
 void rgbWrite(u8 r, u8 g, u8 b);
